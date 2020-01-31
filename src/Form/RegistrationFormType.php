e6b6d689a85090e2bfe5af56d9d417885c0e3932
DarshBadal <darshan2002@hotmail.nl>
2020-01-31 12:55:31 +0100

    knopje dat kwijt was is weer terug

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,7 +22,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Accepteer de termen.',
                     ]),
                 ],
             ])
@@ -36,7 +36,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Je wachtwoord moet langer dan  {{ limit }} karakters zijn',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
